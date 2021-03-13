21bb52aa00ff200d8066878b8d50cc99370988f5
SnowSuno <percy3368@gmail.com>
2021-03-13 15:16:35 +0900

    Initial commit

---
+++ run.py
@@ -0,0 +1,21 @@
+import time
+
+def present_time():
+    now = time.localtime()
+    return "[%04d/%02d/%02d %02d:%02d:%02d] " % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+
+
+while True:
+    try:
+        print(present_time() + 'Server start')
+        import Main
+
+    except ConnectionResetError:
+        pass
+
+    except Exception as e:
+        print(present_time() + e)
+
+    print(present_time() + 'Connection Reset')
+    time.sleep(5)
+
