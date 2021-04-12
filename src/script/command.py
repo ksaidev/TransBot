b0d862de513eaab219a440e036a113f8abfb46ac
SnowSuno <percy3368@gmail.com>
2021-04-12 15:04:04 +0900

    appended admin commands

--- src/script/command.py
+++ src/script/command.py
@@ -15,12 +15,18 @@ class Command:
     AUTO_TRANSLATE = Type('auto_translate')
     MANUAL_TRANSLATE = Type('manual_translate')
 
+    SYNC = Type('sync')
+
     settings = {
         ('도움말',): HELP_KO, ('help',): HELP_EN,
         ('등록', 'reg'): REGISTER,
         ('자동', 'auto', 'ㅈ', 'a'): SET_AUTO,
         ('수동', 'manual', 'ㅅ', 'm'): SET_MANUAL,
         ('번역', 'translate', 'ㅂ', 'tr'): MANUAL_TRANSLATE
+    }
+
+    admin_settings = {
+        ('동기화', 'sync'): SYNC
     }
 
     functions = dict()
@@ -33,15 +39,20 @@ class Command:
         return command_func
 
     @staticmethod
-    def _get_type(message):
+    def _get_type(message, is_admin):
         if message.startswith('/'):
             command = message[1:]
+            if is_admin:
+                for admin_commands in Command.admin_settings:
+                    if command in admin_commands:
+                        return Command.admin_settings[admin_commands]
+
             for commands in Command.settings:
                 if command in commands:
                     return Command.settings[commands]
         return Command.AUTO_TRANSLATE
 
     @staticmethod
-    def get_function(message):
-        command_type = Command._get_type(message)
+    def get_function(message, admin=False):
+        command_type = Command._get_type(message, admin)
         return Command.functions[command_type]
