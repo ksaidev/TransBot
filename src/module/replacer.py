e2068e57210971b913a9f9439ba38d0fc413c7d5
SnowSuno <percy3368@gmail.com>
2021-05-21 15:43:57 +0900

    temp commit

--- src/module/replacer.py
+++ src/module/replacer.py
@@ -1,2 +1,28 @@
+from src.module.flashtext import KeywordProcessor
+
 class Replacer:
-    def in
+    def __init__(self):
+        db_dir = '../data/words.json'
+        self.preprocessor = {
+            'ko': KeywordProcessor(),
+            'en': KeywordProcessor()
+        }
+
+
+
+
+
+
+# from old flashtext
+
+# def load_from_database(self):
+#     try:
+#         with open(self.db_dir, 'r') as fp:
+#             data = json.load(fp)
+#     except json.decoder.JSONDecodeError:
+#         data = {}
+#     self.keyword_trie_dict = data
+#
+# def save_to_database(self):
+#     with open(self.db_dir, 'w') as fp:
+#         json.dump(self.keyword_trie_dict, fp, indent='\t')
