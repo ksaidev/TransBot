c29b1a3be4ca0185b5f389aeda374174d8fa6a76
SnowSuno <percy3368@gmail.com>
2021-05-24 16:20:39 +0900

    deleted unused scripts

--- flashtext_test.py
+++
@@ -1,9 +0,0 @@
-from flashtext.keyword import KeywordProcessor
-
-replacer = KeywordProcessor()
-replacer.add_keywords_from_dict({'ksa': ['한과영', 'korea']})
-
-
-print(replacer.replace_keywords('한과영은 좋은 학교이다'))
-print(replacer.keyword_trie_dict)
-
